d3ea26240d39a011139b61f1007f1835b3237eb8
Yongha Hwang <yongha.hwang@gmail.com>
2017-05-17 12:55:25 -0400

    fix for mac environment

--- src/floor_xml.py
+++ src/floor_xml.py
@@ -1,14 +1,18 @@
 #! /usr/bin/env python
 
 import mapnik
-import cairo
+# import cairo
+import sys
 
 
-stylesheet = 'floor.xml'
-image = '../image/floor.png'
-m = mapnik.Map(2600, 1300)
+stylesheet = sys.argv[1]
+w = int(sys.argv[2])
+h = int(sys.argv[3])
+filename = stylesheet.split('/')[-1]
+image = '../image/{}.png'.format(filename)
+m = mapnik.Map(w, h)
 mapnik.load_map(m, stylesheet)
-m.zoom_all() 
+m.zoom_all()
 mapnik.render_to_file(m, image)
 print("rendered image to {}".format(image))
 
